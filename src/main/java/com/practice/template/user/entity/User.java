bff7696d00452848aa20dcc3255d35e6ebe582fa
lhseunge <lhseunge@gmail.com>
2025-02-18 20:13:48 +0900

    회원 Entity 추가

---
+++ src/main/java/com/practice/template/user/entity/User.java
@@ -0,0 +1,20 @@
+package com.practice.template.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private String phone;
+
+}
