4d35ecc29292be5846813d6f3ce9242afc52a163
lhseunge <lhseunge@gmail.com>
2025-02-26 18:24:07 +0900

    API /users/{id} 추가

--- src/main/java/com/practice/template/domain/user/controller/UserController.java
+++ src/main/java/com/practice/template/domain/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.practice.template.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +18,8 @@ public class UserController {
         return ResponseEntity.ok().body(userService.getUsers());
     }
 
+    @GetMapping("users/{id}")
+    public ResponseEntity<?> getUser(@PathVariable("id") Long id) {
+        return ResponseEntity.ok().body(userService.getUserById(id));
+    }
 }
