3a2b741928b2cbac72226210e9399b44e7507688
lhseunge <lhseunge@gmail.com>
2025-02-26 18:24:49 +0900

    UserService.getUserById 인터페이스 추가 및 구현

--- src/main/java/com/practice/template/domain/user/service/UserService.java
+++ src/main/java/com/practice/template/domain/user/service/UserService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface UserService {
 
     List<UserResponse> getUsers();
+
+    UserResponse getUserById(Long id);
 }
--- src/main/java/com/practice/template/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/practice/template/domain/user/service/UserServiceImpl.java
@@ -1,22 +1,42 @@
 package com.practice.template.domain.user.service;
 
+import com.practice.template.domain.user.entity.User;
 import com.practice.template.domain.user.repository.UserRepository;
 import com.practice.template.domain.user.dto.UserResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static com.practice.template.domain.user.entity.QUser.user;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponse::from)
                 .toList();
+    }
+
+    @Override
+    public UserResponse getUserById(Long id) {
+
+        User userInfo = queryFactory.selectFrom(user)
+                .where(user.id.eq(id))
+                .fetchOne();
+
+        if (userInfo == null) {
+            throw new RuntimeException("user not found");
+        }
+
+        return UserResponse.from(userInfo);
+
     }
 }
