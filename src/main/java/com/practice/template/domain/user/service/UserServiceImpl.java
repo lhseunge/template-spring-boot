286d486dea355160a889b0b90d5faa6f9fdfe7d6
lhseunge <lhseunge@gmail.com>
2025-06-11 20:31:28 +0900

    chore: 미사용 Querydsl import 제거

--- src/main/java/com/practice/template/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/practice/template/domain/user/service/UserServiceImpl.java
@@ -1,22 +1,18 @@
 package com.practice.template.domain.user.service;
 
+import com.practice.template.domain.user.dto.UserResponse;
 import com.practice.template.domain.user.entity.User;
 import com.practice.template.domain.user.repository.UserRepository;
-import com.practice.template.domain.user.dto.UserResponse;
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static com.practice.template.domain.user.entity.QUser.user;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
-    private final JPAQueryFactory queryFactory;
 
     @Override
     public List<UserResponse> getUsers() {
