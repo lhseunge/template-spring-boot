414d422ca9b897c770035a4491c61a2f7759f667
lhseunge <lhseunge@gmail.com>
2025-06-16 10:44:22 +0900

    refactor: ResponseEntity 제네릭 물음표 수정

--- src/main/java/com/practice/template/domain/user/controller/UserController.java
+++ src/main/java/com/practice/template/domain/user/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.practice.template.domain.user.controller;
 
+import com.practice.template.domain.user.dto.UserInfo;
 import com.practice.template.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,12 +17,12 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("users")
-    public ResponseEntity<?> getUsers() {
+    public ResponseEntity<List<UserInfo>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
 
     @GetMapping("users/{id}")
-    public ResponseEntity<?> getUser(@PathVariable("id") Long id) {
+    public ResponseEntity<UserInfo> getUser(@PathVariable("id") Long id) {
         return ResponseEntity.ok().body(userService.getUserById(id));
     }
 }
